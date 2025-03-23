cc4d989c07648c0f2384e98e7ce0b1a38999495f
boyiwei <wby@princeton.edu>
2025-03-23 19:58:56 +0000

    fixed main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from getpass import getuser
 def run(input: dict[str, dict], **kwargs) -> dict[str, str]:
     # SWE agent can only run on swebench
     assert 'model_name' in kwargs, 'model_name is required'
+    assert len(input) == 1, 'input must contain only one task'
     
     # Set default values for kwargs
     per_instance_cost_limit = kwargs.get('per_instance_cost_limit', 3.0)
@@ -14,14 +15,13 @@ def run(input: dict[str, dict], **kwargs) -> dict[str, str]:
     top_p = kwargs.get('top_p', '0.95')
     temperature = kwargs.get('temperature', '0.00')
     config_file = kwargs.get('config_file', Path(__file__).resolve().parent / "config" / "default.yaml")
-    
     path = Path(__file__).resolve().parent / "input.json"
-    # write the input to json file
+    
+    
+    # Reformat the input.json file to make it compatible with the SWE-agent
     tasks = []
     for instance_id, task in input.items():
         tasks.append(task)
-    
-
     with open(path, "w") as f:
         json.dump(tasks, f, indent=4)
 
@@ -39,26 +39,24 @@ def run(input: dict[str, dict], **kwargs) -> dict[str, str]:
     
     traj_dir = main(args) / "all_preds.jsonl"
     
-    import os
-    os.remove(path)
     
     with open(traj_dir) as f:
         data = list(f)
-    model_name_or_path = kwargs['model_name']
     keys = list(input.keys())
-
+    print(keys)
+    
+    model_patch = None
     for idx, key in enumerate(keys):
         for line in data:
             line = json.loads(line)
             instance = input[key]
-            
+            print(instance)
+            print(line)
+
             if instance['instance_id'] == line['instance_id']:
                 print("find matching instance!")
-                instance['model_name_or_path'] = line['model_name_or_path']
-                instance['model_patch'] = line['model_patch']
+                model_patch = line['model_patch']
                 break
-
-            instance['model_name_or_path'] = model_name_or_path
-            instance['model_patch'] = 'No patch returned'
-
-    return input
+    
+    print(model_patch)
+    return {input[keys[0]]['instance_id']: model_patch}
